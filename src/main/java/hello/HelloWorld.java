26ec805ab0458cd47cdd0fbb4e3a6e58f3a93a6d
周啟芸 <1904017109@qq.com>
2018-02-28 12:00:17 +0800

    helloworld

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+/**
+ * Created on 2018/2/28.
+ *
+ * @author qiyun
+ */
+public class HelloWorld {
+  public static void main(String args[]){
+    System.out.println("hello world!");
+  }
+}
